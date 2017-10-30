8a668ff583ec3ec2bafdc07cba04ac3c02b27202
MasterUNG <phrombutr@gmail.com>
2017-10-30 12:32:58 +0700

    Sign UP OK

---
+++ app/src/main/java/rmutsb/mook/chatchon/makingfavorcoffee/ultility/MyConstant.java
@@ -0,0 +1,14 @@
+package rmutsb.mook.chatchon.makingfavorcoffee.ultility;
+
+/**
+ * Created by masterung on 30/10/2017 AD.
+ */
+
+public class MyConstant {
+
+    private String urlPostUserString = "http://androidthai.in.th/nut/addUserNut.php";
+
+    public String getUrlPostUserString() {
+        return urlPostUserString;
+    }
+}
