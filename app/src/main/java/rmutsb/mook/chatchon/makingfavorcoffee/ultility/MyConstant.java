133daa6e2301018c702a385a7c7fad18e642db5e
MasterUNG <phrombutr@gmail.com>
2017-10-30 14:06:34 +0700

    Auten OK

--- app/src/main/java/rmutsb/mook/chatchon/makingfavorcoffee/ultility/MyConstant.java
+++ app/src/main/java/rmutsb/mook/chatchon/makingfavorcoffee/ultility/MyConstant.java
@@ -7,6 +7,18 @@ package rmutsb.mook.chatchon.makingfavorcoffee.ultility;
 public class MyConstant {
 
     private String urlPostUserString = "http://androidthai.in.th/nut/addUserNut.php";
+    private String urlGetUserString = "http://androidthai.in.th/nut/getAllDataNut.php";
+
+    private String[] columnUserStrings = new String[]{"user_id", "user_Name",
+            "user_Surname", "user_Phone", "user_Email", "user_Password"};
+
+    public String[] getColumnUserStrings() {
+        return columnUserStrings;
+    }
+
+    public String getUrlGetUserString() {
+        return urlGetUserString;
+    }
 
     public String getUrlPostUserString() {
         return urlPostUserString;
